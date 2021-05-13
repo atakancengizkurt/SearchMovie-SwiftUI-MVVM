3abc74aab68a17c0c393632ab91f83918839d629
Atakan Cengiz KURT <atakan.kurt@loodos.com>
2021-05-13 18:41:36 +0300

    Initial Commit

---
+++ SearchMovie-SwiftUI-MVVM/SearchMovie_SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchMovie_SwiftUI_MVVMApp.swift
+//  SearchMovie-SwiftUI-MVVM
+//
+//  Created by Atakan Cengiz KURT on 13.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct SearchMovie_SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
